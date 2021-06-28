88630aa529a920b6aa3346abdd2517915c75b68a
hackerassociate <62351511+hackerassociate@users.noreply.github.com>
2021-06-28 23:13:38 +0530

    Create Offen_Harsh_Vuln.sh

---
+++ Offen_Harsh_Vuln.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+echo -e "\n
+-----------------------------------------------------------------------------------------------------
+			                Welcome to Offensive IOT by Harshad!
+                            
+                	    Harsh BOT Ready for Vulnerability Scanning. 
+                            
+                      Provide  IP  for Vulnerability  Scanning.
+			    
+                      #Author = Harshad Shah
+-----------------------------------------------------------------------------------------------------"
+
+echo -e "\nWelcome to Offensive IOT Scanning Program!\n"
+
+echo -e "\n"
+
+sudo apt-get install nikto -y ; echo -e "\n"
+
+echo -e "\nScan Tuning Options!\n"
+
+echo -e "\n"
+
+sudo nikto -H 
+
+echo -e "\n"
+
+echo -e "\n"
+
+read -p "Enter the  IP/WEB  for Vulnerability Scanning (eg. www.xyz.com) : " IP_ADDR
+
+
+echo -e "\n"
+
+echo -e "\n"
+
+nikto -h   $IP_ADDR -o $IP_ADDR.html -F html ; echo -e "\n" ; echo -e "\n"
+
+echo -e "\n"
+
+echo -e "\n"
+
+echo -e  "\nThank You Scanning done!\n"
